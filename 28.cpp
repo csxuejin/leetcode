0b8612fc0471ecdc6d11a1ef9e0a8943406d3a59
csxuejin <602296435@qq.com>
2016-07-04 20:09:53 +0800

    Implement strStr()

---
+++ 28.cpp
@@ -0,0 +1,21 @@
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int len = needle.size();
+        if (len == 0){
+            return 0;
+        }
+        char firCh = needle[0];
+        for (int i=0; i<haystack.size(); i++){
+            char ch = haystack[i];
+            if (ch == firCh){
+                string str = haystack.substr(i,len);
+                if (str == needle){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+};
