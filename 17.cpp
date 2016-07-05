ab8057957265b48d4f9898e6950005b6194c3109
csxuejin <602296435@qq.com>
2016-07-05 14:21:46 +0800

    Letter Combinations of a Phone Number

---
+++ 17.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> num{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    vector<string> res;
+    vector<string> letterCombinations(string digits) {
+        if (digits.length()>0)
+            helper("", digits);
+        return res;
+    }
+    
+    void helper(string tmp, string dig){
+        if (dig.length() == 0){
+            res.push_back(tmp);
+            return;
+        }
+        string str = num[dig[0]-'0'];
+        string subStr = dig.substr(1, dig.length()-1);
+        for (int i=0; i<str.length(); i++){
+            helper(tmp + str[i], subStr);
+        }
+    }
+    
+    
+};
