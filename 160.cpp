ca92545fd6c04985c23230753579651ee9b674fd
csxuejin <602296435@qq.com>
2016-07-11 19:52:09 +0800

    Intersection of Two Linked Lists

---
+++ 160.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+       ListNode *p1 = headA;
+       ListNode *p2 = headB;
+       if(!p1 || !p2) return NULL;
+       while(p1 && p2 && p1!=p2){
+           p1 = p1->next;
+           p2 = p2->next;
+           if(p1 == p2) return p1;
+           if(!p1) p1 = headB;
+           if(!p2) p2 = headA;
+       }
+       return p1;
+    }
+};
