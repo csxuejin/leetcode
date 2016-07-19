a32a274233f409182f9bf8fa855db46dc649be6b
csxuejin <602296435@qq.com>
2016-07-19 10:34:41 +0800

    Convert Sorted List to Binary Search Tree

---
+++ 109.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        if(!head) return NULL;
+        if(!head->next) return new TreeNode(head->val);
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while(fast && fast->next){
+             slow = slow->next;
+             fast = fast->next->next;
+        }
+        ListNode *t = head;
+        while(t->next != slow){
+            t = t->next;
+        }
+        t->next = NULL;
+        TreeNode *root = new TreeNode(slow->val);
+        root->left = sortedListToBST(head);
+        root->right = sortedListToBST(slow->next);
+        return root;
+    }
+};
+
