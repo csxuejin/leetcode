5c56e9dbd49f48eaf07786d62b2ac46734be4b0d
csxuejin <602296435@qq.com>
2016-07-27 16:30:50 +0800

    Longest Increasing Subsequence

---
+++ 300.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+       if(nums.empty())
+            return 0;
+       vector<int> vec;
+       for(int i=0; i<nums.size(); i++){
+           auto it = lower_bound(vec.begin(), vec.end(), nums[i]);
+           if(it==vec.end()){
+               vec.push_back(nums[i]);
+           }else{
+               *it = nums[i];
+           }
+       }
+       return vec.size();
+    }
+};
+
+/*
+// DP: O(n*n);
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        if(nums.empty()) 
+            return 0;
+        vector<int> LIS(nums.size(), 1);
+        int res = 1;
+        for(int i=0; i<nums.size(); i++){
+            for(int j=0; j<i; j++){
+                if(nums[i]>nums[j]){
+                    LIS[i] = max(LIS[i], LIS[j]+1);
+                }
+            }
+            if(LIS[i]>res){
+                res = LIS[i];
+            }
+        }
+        return res;
+    }
+};
+*/
