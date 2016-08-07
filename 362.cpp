725c2733546263f3a9d2e1f7e52621c672fe5baa
csxuejin <602296435@qq.com>
2016-08-07 11:27:21 +0800

    Design Hit Counter

---
+++ 362.cpp
@@ -0,0 +1,30 @@
+class HitCounter {
+private:
+    multiset<int> ss;
+public:
+    /** Initialize your data structure here. */
+    HitCounter() {
+        
+    }
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    void hit(int timestamp) {
+        ss.insert(timestamp);
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    int getHits(int timestamp) {
+        auto begin = ss.lower_bound(timestamp-300+1>0? timestamp-300+1:0);
+        auto end = ss.upper_bound(timestamp);
+        return distance(begin, end);
+    }
+};
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */
