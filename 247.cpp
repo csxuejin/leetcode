7e6f43018285a8be9acb7718bccff7e7543b3df0
csxuejin <602296435@qq.com>
2016-08-15 19:59:14 +0800

    Strobogrammatic Number ||

---
+++ 247.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> findStrobogrammatic(int n) {
+        return helper(n , n);
+    }
+    vector<string> helper(int m, int n){
+        if(m == 0) return vector<string>({""});
+        if(m == 1) return vector<string>({"0", "1", "8"});
+        vector<string> tmp = helper(m - 2, n);
+        vector<string> res;
+        for(int i = 0; i < tmp.size(); i++){
+            if(m != n) res.push_back("0" + tmp[i] + "0"); //最外层两个数不能为0
+            res.push_back("1" + tmp[i] + "1");
+            res.push_back("6" + tmp[i] + "9");
+            res.push_back("8" + tmp[i] + "8");
+            res.push_back("9" + tmp[i] + "6");
+        }
+        return res;
+    }
+};
+
