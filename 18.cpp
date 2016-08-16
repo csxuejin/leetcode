8f49bae01215317838b6e784cb4ca5064ca39e7d
csxuejin <602296435@qq.com>
2016-08-16 10:32:50 +0800

    4Sum

--- 18.cpp
+++ 18.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     vector<vector<int> > fourSum(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
-        int i,j;    
+        int i,j, n=nums.size();    
         vector< vector<int> > result;
-        int n=nums.size();
         for(i=0; i<n; i++){
             if(i>0 && nums[i]==nums[i-1])
                 continue;
@@ -18,12 +17,8 @@ public:
                 while(left<right){
                     int t = nums[left] + nums[right] + s;                
                     if(t == target){
-                        while(left+1<n && nums[left]==nums[left+1]){
-                            left++;
-                        }
-                        while(right-1>=0 && nums[right]==nums[right-1]){
-                            right--;
-                        }
+                        while(left+1<n && nums[left]==nums[left+1]) left++;
+                        while(right-1>left && nums[right]==nums[right-1]) right--;
                         result.push_back(vector<int>{nums[i], nums[j], nums[left], nums[right]});  
                         left++;
                         right--;
