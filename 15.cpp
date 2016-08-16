934bcf5aaa995c39385fc5da076afd8bc8211afb
csxuejin <602296435@qq.com>
2016-08-16 10:13:58 +0800

    3Sum

--- 15.cpp
+++ 15.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         vector<vector<int>> res;
-        int n = nums.size();
-        for(int i=0; i<n; i++){
+        if(nums.size()<3) return res;
+        sort(nums.begin(), nums.end());
+        
+        for(int i=0; i<nums.size()-2; i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
-            int l,r;
-            l = i+1;
-            r = nums.size()-1;
+            int l = i+1;
+            int r = nums.size()-1;
             while(l<r){
-                int tmp = nums[i] + nums[l] + nums[r];
-                if(tmp == 0){
-                    while(l+1<n && nums[l]==nums[l+1]){
-                        l++;
-                    }
-                    while(r-1>=0 && nums[r]==nums[r-1]){
-                        r--;
-                    }
+                int t = nums[i]+nums[l]+nums[r];
+                if(t==0){
+                    while(l+1<nums.size()-1 && nums[l]==nums[l+1]) l++;
+                    while(r-1>l && nums[r]==nums[r-1]) r--;
                     res.push_back(vector<int>{nums[i], nums[l], nums[r]});
                     l++;
                     r--;
-                }else if(tmp>0){
+                }else if(t>0){
                     r--;
                 }else{
                     l++;
