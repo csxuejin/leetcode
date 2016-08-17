29400ae2f11fc4bd330bd3480d3db70912993947
csxuejin <602296435@qq.com>
2016-08-17 20:41:45 +0800

    Valid Palindrome

--- 125.cpp
+++ 125.cpp
@@ -1,17 +1,41 @@
+// o(log(n)) solution
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string str = "";
-        for (int i=0; i<s.size(); i++){
-            if(isalnum(s[i])){
-                if (isalpha(s[i])){
-                   s[i] = tolower(s[i]);
-                }
-                str += s[i];
-            }
+        int l=0, r=s.size()-1;
+        while(l<r){
+            while(l<r && !isalnum(s[l])) l++;
+            if(l>=r) break;
+            s[l] = isalpha(s[l])? tolower(s[l]):s[l];
+            
+            while(l<r && !isalnum(s[r])) r--;
+            if(l>=r) break;
+            s[r] = isalpha(s[r])? tolower(s[r]):s[r];
+            
+            if(s[l]!=s[r]) return false;
+            l++;
+            r--;
         }
-        string tmp = str;
-        reverse(str.begin(), str.end());
-        return tmp == str;
+        return true;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+//         string str = "";
+//         for (int i=0; i<s.size(); i++){
+//             if(isalnum(s[i])){
+//                 if (isalpha(s[i])){
+//                    s[i] = tolower(s[i]);
+//                 }
+//                 str += s[i];
+//             }
+//         }
+//         string tmp = str;
+//         reverse(str.begin(), str.end());
+//         return tmp == str;
+//     }
+// };
