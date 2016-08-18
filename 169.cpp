4ee9f46f1b87256cb66bf76f2649bc4dd5b9664f
csxuejin <602296435@qq.com>
2016-08-18 18:20:01 +0800

    Majority Element

--- 169.cpp
+++ 169.cpp
@@ -1,7 +1,24 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int, int> mm;
+        int res, cnt = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(cnt==0){
+                res = nums[i];
+                cnt = 1;
+            }else{
+                cnt = res==nums[i]? cnt+1:cnt-1;
+            }
+        }
+        return res;
+    }
+};
+
+/*
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> mm;
         int res;
         for (int i=0; i<nums.size(); i++){
             if(++mm[nums[i]] > nums.size()/2){
@@ -12,4 +29,5 @@ public:
         return res;
     }
 };
+*/
 
