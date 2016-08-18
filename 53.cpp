382ffb01e885de760210d1e6ca69538598fa63c4
csxuejin <602296435@qq.com>
2016-08-18 19:07:04 +0800

    Maximum Subarray

--- 53.cpp
+++ 53.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        if(nums.size() == 0) 
+        if(nums.empty()) 
             return 0;
         int maxSum = INT_MIN, curSum = 0;
         for (int i=0; i<nums.size(); i++){
