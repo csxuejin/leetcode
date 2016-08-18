80a6635f5186db63be94337c60ed0fe8b2d9c539
csxuejin <602296435@qq.com>
2016-08-18 18:40:19 +0800

    Best Time to Buy and Sell Stock

--- 121.cpp
+++ 121.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        if(prices.size() == 0){
+        if(prices.empty()) 
             return 0;
-        }
-        int tMin = prices[0];
-        int tMax = INT_MIN;
+
+        int tMin = prices[0], tMax = INT_MIN;
         for (int i=0; i<prices.size(); i++){
-            if(prices[i] < tMin ){
-                tMin = prices[i];
-            }
-            if(prices[i]-tMin > tMax){
-                tMax = prices[i] - tMin;
-            }
+            tMin = min(tMin, prices[i]);
+            tMax = max(tMax, prices[i]-tMin);
         }
         return tMax;
     }
