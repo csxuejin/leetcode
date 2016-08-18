b9868effc63fb5f193068dc14b09a4b7f40cbff0
csxuejin <602296435@qq.com>
2016-08-18 11:15:38 +0800

    Sort Colors

--- 75.cpp
+++ 75.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int low, mid, high;
-        low = 0;
-        mid = 0; 
+        low = mid = 0; 
         high = nums.size()-1;
         while(mid <= high){
             if(nums[mid]==1){
