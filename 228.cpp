a97b881de8a087c817eeed87856a8b3baa8e73c0
csxuejin <602296435@qq.com>
2016-08-20 10:48:20 +0800

    Summary Ranges

--- 228.cpp
+++ 228.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
+    void merge(int& first, int& last, vector<string>& res){
+        if(first != last)
+            res.push_back(to_string(first)+"->"+to_string(last));
+        else
+            res.push_back(to_string(first));
+    }
+
     vector<string> summaryRanges(vector<int>& nums) {
+        if(nums.empty()) return vector<string>{};
+        if(nums.size() == 1) return vector<string>{to_string(nums[0])};
+                
         vector<string> res;
-        if(nums.size()== 0){
-            return res;
-        }
-        if(nums.size() == 1){
-            res.push_back(to_string(nums[0]));
-            return res;
-        }
         int first = nums[0], last = nums[0];
         for (int i=1; i<nums.size(); i++){
             if(nums[i] != last + 1){
-                if(first != last)
-                    res.push_back(to_string(first)+"->"+to_string(last));
-                else
-                    res.push_back(to_string(first));
+                merge(first, last, res);
                 first = last = nums[i];
             }else{
                 last = nums[i];
             }
         }
-        if(first != last){
-            res.push_back(to_string(first) + "->" + to_string(last));
-        }else{
-            res.push_back(to_string(first));
-        }
+        merge(first, last, res);
         return res;
     }
 };
+
