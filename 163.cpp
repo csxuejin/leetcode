e0e7deb9b3ab70216ebee83cd02206a244f2f4e0
csxuejin <602296435@qq.com>
2016-08-23 10:33:23 +0800

    Missing Ranges

--- 163.cpp
+++ 163.cpp
@@ -1,36 +1,28 @@
 class Solution {
+private:
+    vector<string> res;
 public:
+    void insert(int lower, int upper){
+        res.push_back(lower==upper? to_string(lower) : to_string(lower)+"->"+to_string(upper));
+    }
+
     vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
         if(nums.empty()) {
-            if(lower==upper){
-                return vector<string>{to_string(lower)};
-            }else{
-                return vector<string>{to_string(lower)+"->"+to_string(upper)};
-            }
+            insert(lower, upper);
+            return res;
         }
             
-        vector<pair<int,int>> vec;
         for(int i=0; i<nums.size(); i++){
             if(lower<nums[i]){
-                vec.push_back({lower, nums[i]-1});
+                insert(lower, nums[i]-1);
                 lower = nums[i]+1;
             }else if(lower==nums[i]){
                 lower++;
             }
         }
         
-        if(lower<=upper){
-            vec.push_back({lower, upper});
-        }
-        
-        vector<string> res;
-        for(auto it:vec){
-            if(it.first==it.second){
-                res.push_back(to_string(it.first));
-            }else{
-                res.push_back(to_string(it.first)+"->"+to_string(it.second));
-            }
-        }
+        if(lower<=upper)
+            insert(lower, upper);
         
         return res;
     }
