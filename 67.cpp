b2b137581d2843433bccb5f33d610798ce49390d
csxuejin <602296435@qq.com>
2016-08-23 21:21:46 +0800

    Add Binary

--- 67.cpp
+++ 67.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        vector<int> vec;
         int t, last=0;
         string str = "";
         for(int i=a.size()-1, j=b.size()-1; i>=0 || j>=0; i--,j--){
             t = (i<0? 0:a[i]-'0') + (j<0? 0:b[j]-'0') + last; 
-            vec.push_back(t%2);
+            str += (t%2)+'0';
             last = t/2;
         }
         if(last!=0){
-            vec.push_back(last);
+            str += last + '0';
         }       
-        for(int i=vec.size()-1; i>=0; i--)  
-            str += vec[i]+'0';
+        reverse(str.begin(), str.end());
         return str;
     }
 };
+
