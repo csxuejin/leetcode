68d297b174491ed9b8d7617df56277e123de7bb3
csxuejin <602296435@qq.com>
2016-12-05 09:30:40 +0800

    Two Sum

--- 1.cpp
+++ 1.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         map<int, int> mm;
-        vector<int> vec;
         for (int i=0; i<nums.size(); i++){
             if( mm.find(target-nums[i]) != mm.end() ){
-                vec.push_back(mm[target-nums[i]]);
-                vec.push_back(i);
-                return vec;
+                return vector<int>{mm[target-nums[i]], i};
             }
             mm[nums[i]] = i;
         }
-        return vec;
+        return vector<int>{};
     }
 };
