90c6a13c94e8e431be6c2cc5b1697aa4bd063a83
csxuejin <602296435@qq.com>
2016-12-10 19:54:45 +0800

    Minimum Path Sum

--- 64.cpp
+++ 64.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int row = grid.size(); 
-        if(row == 0){
-            return 0;
-        }
-        int col = grid[0].size();
-        vector<vector<int>> dp(row, vector<int>(col,0));
-        dp[0][0] = grid[0][0];
-        for(int i=1; i<col; i++){
-            dp[0][i] = dp[0][i-1]+grid[0][i];
-        }
-        for(int i=1; i<row; i++){
-            dp[i][0] = dp[i-1][0]+grid[i][0];
-        }
-        for(int i=1; i<row; i++){
-            for(int j=1; j<col; j++){
-                dp[i][j] = min(dp[i-1][j], dp[i][j-1])  + grid[i][j];
-            }
-        }
-        return dp[row-1][col-1];
+         int row = grid.size();
+         int col = grid[0].size();
+         for(int i=1; i<col; i++){
+             grid[0][i] = grid[0][i]+grid[0][i-1];
+         }
+         for(int i=1; i<row; i++){
+             grid[i][0] = grid[i][0] + grid[i-1][0];
+         }
+         for(int i=1; i<row; i++){
+             for(int j=1; j<col; j++){
+                 grid[i][j] = min(grid[i-1][j], grid[i][j-1]) + grid[i][j];
+             }
+         }
+         return grid[row-1][col-1];
     }
 };
+
+
