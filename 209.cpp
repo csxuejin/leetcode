9cc9cc8260ce4c54f5c10e64892e016114ee9486
csxuejin <602296435@qq.com>
2016-12-11 19:57:20 +0800

    Minimum Size Subarray Sum

--- 209.cpp
+++ 209.cpp
@@ -4,15 +4,13 @@ public:
         int start = 0, sum=0;
         int res = INT_MAX;
         int i = 0;
-        while(i<nums.size()){
-            while(sum<s && i<nums.size()) sum += nums[i++];
-            if(sum>=s){
-              while(sum>=s && start<i){
+        for(int i=0; i<nums.size(); i++){
+            sum += nums[i];
+            while(sum>=s){
+                res = min(res, i-start+1);
                 sum -= nums[start++];
-              }
-              res = min(res, i-start+1);
             }
-        }
+        }    
         return res==INT_MAX? 0:res;
     }
 };
