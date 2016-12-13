c475c0a0a48f5ee7ea33016479cd40d269d05dc2
csxuejin <602296435@qq.com>
2016-12-13 19:50:13 +0800

    Ransom Note

--- 383.cpp
+++ 383.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<int,int> hash;
-        for(int i=0; i<ransomNote.size(); i++){
-            hash[ransomNote[i]]++;
-        }
+        unordered_map<char, int> mm;
         for(int i=0; i<magazine.size(); i++){
-            if(hash.find(magazine[i]) != hash.end()){
-                hash[magazine[i]]--;
-            }
+            mm[magazine[i]]++;
         }
-        for(auto it:hash){
-            if(it.second>0) return false;
+        for(int i=0; i<ransomNote.size(); i++){
+            if(mm.find(ransomNote[i]) == mm.end() || mm[ransomNote[i]]<1){
+                return false;
+            }
+            mm[ransomNote[i]]--;
         }
         return true;
     }
 };
+
+
+// method 2
+// class Solution {
+// public:
+//     bool canConstruct(string ransomNote, string magazine) {
+//         unordered_map<int,int> hash;
+//         for(int i=0; i<ransomNote.size(); i++){
+//             hash[ransomNote[i]]++;
+//         }
+//         for(int i=0; i<magazine.size(); i++){
+//             if(hash.find(magazine[i]) != hash.end()){
+//                 hash[magazine[i]]--;
+//             }
+//         }
+//         for(auto it:hash){
+//             if(it.second>0) return false;
+//         }
+//         return true;
+//     }
+// };
+
