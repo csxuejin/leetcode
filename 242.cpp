959719d8cec0368eeaf38dd4507be7269c4faafc
csxuejin <602296435@qq.com>
2016-12-18 22:04:14 +0800

    Valid Anagram

--- 242.cpp
+++ 242.cpp
@@ -6,3 +6,27 @@ public:
         return s == t;
     }
 };
+
+/*
+// method 2:
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char, int> mm;
+        if(s.size() != s.size())
+            return false;
+        for (int i=0; i<s.size(); i++){
+            mm[s[i]]++;
+        }
+        for (int i=0; i<t.size(); i++){
+            mm[t[i]]--;
+        }
+        for (auto p : mm){
+            if(p.second!=0)
+                return false;
+        }
+        return true;
+    }
+};
+
+*/
