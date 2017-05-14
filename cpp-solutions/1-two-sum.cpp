ef7f4239cd8189056fb20df0ffb31b1d0f65af8a
csxuejin <602296435@qq.com>
2017-05-14 17:48:21 +0800

    Hamming Distance

---
+++ cpp-solutions/1-two-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> val2pos; //val map to position
+        for(int i=0; i<nums.size(); i++){
+            int rest = target - nums[i];
+            if ( val2pos.find(rest) != val2pos.end() ){
+                return vector<int>{ val2pos[rest], i};
+            }
+            val2pos[nums[i]] = i;
+        }
+        return vector<int>{};
+    }
+};
---
+++ go-solutions/461-hamming-distance.go
@@ -0,0 +1,9 @@
+func hammingDistance(x int, y int) int {
+	s := x ^ y
+	res := 0
+	for s != 0 {
+		s &= s - 1
+		res++
+	}
+	return res
+}
