ddff4ef6df7ef7378f81f8c05c5fa0067a148331
csxuejin <602296435@qq.com>
2018-03-06 14:16:47 +0800

    add 1.md

---
+++ SUMMARY.md
@@ -0,0 +1,5 @@
+# Summary
+
+* [Introduction](README.md)
+* [Algorithms](algorithms/README.md)
+    * [1. Two Sum](algorithms/1.md)
--- cpp-solutions/64.cpp
+++ cpp-solutions/64.cpp
@@ -1,21 +1,25 @@
-class Solution {
-public:
-    int minPathSum(vector<vector<int>>& grid) {
-         int row = grid.size();
-         int col = grid[0].size();
-         for(int i=1; i<col; i++){
-             grid[0][i] = grid[0][i]+grid[0][i-1];
-         }
-         for(int i=1; i<row; i++){
-             grid[i][0] = grid[i][0] + grid[i-1][0];
-         }
-         for(int i=1; i<row; i++){
-             for(int j=1; j<col; j++){
-                 grid[i][j] = min(grid[i-1][j], grid[i][j-1]) + grid[i][j];
-             }
-         }
-         return grid[row-1][col-1];
+class Solution
+{
+  public:
+    int minPathSum(vector<vector<int>> &grid)
+    {
+        int row = grid.size();
+        int col = grid[0].size();
+        for (int i = 1; i < col; i++)
+        {
+            grid[0][i] = grid[0][i] + grid[0][i - 1];
+        }
+        for (int i = 1; i < row; i++)
+        {
+            grid[i][0] = grid[i][0] + grid[i - 1][0];
+        }
+        for (int i = 1; i < row; i++)
+        {
+            for (int j = 1; j < col; j++)
+            {
+                grid[i][j] = min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
+            }
+        }
+        return grid[row - 1][col - 1];
     }
 };
-
-
