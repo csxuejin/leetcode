ea1d8e71f49ea5490303b033944a26df10f223eb
csxuejin <602296435@qq.com>
2018-07-07 16:31:41 +0800

    add solution for 637

---
+++ go-solutions/637.go
@@ -0,0 +1,43 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type record struct {
+	val float64
+	num int
+}
+
+func averageOfLevels(root *TreeNode) []float64 {
+	res := make([]record, 0)
+	helper(root, &res, 1)
+
+	r := make([]float64, 0)
+	for _, v := range res {
+		r = append(r, v.val/float64(v.num))
+	}
+
+	return r
+}
+
+func helper(root *TreeNode, res *[]record, level int) {
+	if root == nil {
+		return
+	}
+
+	if len(*res) < level {
+		*res = append(*res, record{})
+	}
+
+	(*res)[level-1].val += float64(root.Val)
+	(*res)[level-1].num += 1
+
+	helper(root.Left, res, level+1)
+	helper(root.Right, res, level+1)
+}
