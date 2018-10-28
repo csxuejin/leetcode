a737d02bff4cda0b6707c2ecc15173377eb7c05c
csxuejin <602296435@qq.com>
2018-10-28 16:48:59 +0800

    modify solution for 206

--- go-solutions/206.go
+++ go-solutions/206.go
@@ -1,54 +1,31 @@
-package main
-
-import "fmt"
-
-//Definition for singly-linked list.
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
-
-func main() {
-	head := &ListNode{-1, nil}
-	head.Val = -1
-	tmp := head
-	fmt.Println(tmp.Val)
-	for i := 0; i < 5; i++ {
-		newNode := &ListNode{0, nil}
-		newNode.Val = i
-		tmp.Next = newNode
-		tmp = tmp.Next
-	}
-
-	head = reverseList(head)
-	fmt.Println("!!!")
-	for head != nil {
-		fmt.Println(head.Val)
-		head = head.Next
-	}
-}
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
 
 func reverseList(head *ListNode) *ListNode {
 	if head == nil {
-		return head
+		return nil
 	}
 
-	now := head
-	next := head.Next
-	//count := 1
-	fmt.Println("place 1")
-	for next != nil {
-		//count++
-		//fmt.Println(count)
-		tmp := next.Next
-		next.Next = now
-		now = next
-		next = tmp
+	prv := &ListNode{
+		Val:  head.Val,
+		Next: nil,
 	}
-	fmt.Println("here")
-	for now != nil {
-		fmt.Println(now.Val)
-		now = now.Next
+
+	head = head.Next
+	for head != nil {
+		tmp := &ListNode{
+			Val:  head.Val,
+			Next: prv,
+		}
+
+		prv = tmp
+		head = head.Next
 	}
-	return now
+
+	return prv
 }
