36033afe7eba1998a34799e7754353bd762adee7
csxuejin <602296435@qq.com>
2019-08-01 22:09:19 +0800

    add code for 226

---
+++ go-solutions/226/main.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	root.Left, root.Right = root.Right, root.Left
+
+	invertTree(root.Left)
+	invertTree(root.Right)
+
+	return root
+}
