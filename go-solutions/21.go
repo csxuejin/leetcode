f8ba9da98f631ac06a870e56760201a5b24303c0
csxuejin <602296435@qq.com>
2020-02-13 22:45:02 +0800

    add code

--- go-solutions/21.go
+++ go-solutions/21.go
@@ -1,3 +1,5 @@
+package main
+
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
@@ -6,34 +8,38 @@
  * }
  */
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-	if l1 == nil {
-		return l2
-	}
-	if l2 == nil {
-		return l1
-	}
-	var head = &ListNode{0, nil}
-	var tmp *ListNode
-	tmp = head
+	cur := &ListNode{}
+	head := cur
+
 	for l1 != nil && l2 != nil {
 		if l1.Val < l2.Val {
-			tmp.Next = l1
+			cur.Next = l1
 			l1 = l1.Next
 		} else {
-			tmp.Next = l2
+			cur.Next = l2
 			l2 = l2.Next
 		}
-		tmp = tmp.Next
+		cur = cur.Next
 	}
 
-	if l1 != nil {
-		tmp.Next = l1
+	if l1 == nil {
+		cur.Next = l2
 	} else {
-		tmp.Next = l2
+		cur.Next = l1
 	}
 	return head.Next
 }
 
-
-
-
+// method 2: use recursion
+//func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+//	if l1 == nil return l2
+//	if l2 == nil return l1
+//
+//	if l1.Val < l2.Val {
+//		l1.Next = mergeTwoLists(l1.Next, l2)
+//		return l1
+//	}
+//
+//	l2.Next = mergeTwoLists(l2.Next, l1)
+//	return l2
+//}
