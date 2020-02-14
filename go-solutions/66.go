43c1baaf2ba6adf1d5ad10e0e644a03cf5749ddd
csxuejin <602296435@qq.com>
2020-02-14 16:24:14 +0800

    add code

--- go-solutions/66.go
+++ go-solutions/66.go
@@ -1,18 +1,16 @@
+package main
+
 func plusOne(digits []int) []int {
-	res := []int{}
-	last := 1
+	extra := 1
 	for i := len(digits) - 1; i >= 0; i-- {
-		digits[i] += last
-		last = digits[i] / 10
-		digits[i] %= 10
-		res = append(res, digits[i])
-	}
-	if last != 0 {
-		res = append(res, last)
-	}
-	for l, r := 0, len(res)-1; l < r; l, r = l+1, r-1 {
-		res[l], res[r] = res[r], res[l]
+		remain := (digits[i] + extra) % 10
+		extra = (digits[i] + extra) / 10
+		digits[i] = remain
 	}
 
-	return res
+	if extra != 0 {
+		return append([]int{extra}, digits...)
+	}
+
+	return digits
 }
