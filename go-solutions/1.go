7ba9b545d7becf9d5d12d18ac50b44d54c566688
csxuejin <602296435@qq.com>
2020-02-14 16:13:56 +0800

    add new method

--- go-solutions/1.go
+++ go-solutions/1.go
@@ -11,3 +11,37 @@ func twoSum(nums []int, target int) []int {
 	}
 	return nil
 }
+
+// method 2: sort struct slice
+//type Node struct{
+//	index, val int
+//}
+//
+//func twoSum(nums []int, target int) []int {
+//	var nodes []Node
+//	for i, v := range nums{
+//		nodes = append(nodes, Node{
+//			index: i,
+//			val: v,
+//		})
+//	}
+//	sort.Slice(nodes, func(i, j int) bool {
+//		return nodes[i].val< nodes[j].val
+//	})
+//
+//	left, right := 0, len(nums) - 1
+//	for left < right {
+//		val := nodes[left].val + nodes[right].val
+//		switch {
+//		case val == target:
+//			return []int{nodes[left].index, nodes[right].index}
+//
+//		case val < target:
+//			left++
+//
+//		default:
+//			right--
+//		}
+//	}
+//	return nil
+//}
