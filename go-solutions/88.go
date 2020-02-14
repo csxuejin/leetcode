aca885dd6224d85edfe5c9fd61f6ab54c035d798
csxuejin <602296435@qq.com>
2020-02-14 16:00:56 +0800

    add code for 88

--- go-solutions/88.go
+++ go-solutions/88.go
@@ -1,37 +1,27 @@
 package main
 
-import "fmt"
-
-func main() {
-	// nums1 := []int{1, 3, 5, 7}
-	nums1 := make([]int, 1, 2)
-	nums1 = []int{0}
-	// nums2 := []int{2, 4, 6, 8}
-	nums2 := []int{1}
-	merge(nums1, 0, nums2, 1)
-}
-
 func merge(nums1 []int, m int, nums2 []int, n int) {
-	nums := []int{}
-	i, j := 0, 0
-	for i < m && j < n {
-		if nums1[i] < nums2[j] {
-			nums = append(nums, nums1[i])
-			i++
-		} else {
-			nums = append(nums, nums2[j])
-			j++
-		}
-	}
+	cur := m + n - 1
+	i, j := m-1, n-1
+	for i >= 0 || j >= 0 {
+		switch {
+		case i < 0:
+			nums1[cur] = nums2[j]
+			j--
 
-	for i < m {
-		nums = append(nums, nums1[i])
-		i++
+		case j < 0:
+			nums1[cur] = nums1[i]
+			i--
+
+		default:
+			if nums1[i] > nums2[j] {
+				nums1[cur] = nums1[i]
+				i--
+			} else {
+				nums1[cur] = nums2[j]
+				j--
+			}
+		}
+		cur--
 	}
-	for j < n {
-		nums = append(nums, nums2[j])
-		j++
-	}
-	nums1 = nums
-	fmt.Println(nums1)
 }
