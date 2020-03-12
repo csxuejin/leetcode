144b4e3792849c35b67a79ac124614e59ad86466
csxuejin <602296435@qq.com>
2020-03-12 23:10:52 +0800

    add code for 53

---
+++ go-solutions/53.go
@@ -0,0 +1,24 @@
+package main
+
+func maxSubArray(nums []int) int {
+	if len(nums) < 1 {
+		return 0
+	}
+
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	res := dp[0]
+	for i := 1; i < len(nums); i++ {
+		dp[i] = max(dp[i-1], 0) + nums[i]
+		res = max(res, dp[i])
+	}
+
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
