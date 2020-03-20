94e8fc75d175d862604a7e05b27064a7996356a5
csxuejin <602296435@qq.com>
2020-03-20 15:09:47 +0800

    rename params

--- go-solutions/121.go
+++ go-solutions/121.go
@@ -1,14 +1,17 @@
+package main
+
 func maxProfit(prices []int) int {
 	if len(prices) <= 1 {
 		return 0
 	}
-	min := 0
+
+	res := 0
 	for i := 0; i < len(prices)-1; i++ {
 		for j := i + 1; j < len(prices); j++ {
-			if (prices[j] - prices[i]) > min {
-				min = prices[j] - prices[i]
+			if (prices[j] - prices[i]) > res {
+				res = prices[j] - prices[i]
 			}
 		}
 	}
-	return min
+	return res
 }
