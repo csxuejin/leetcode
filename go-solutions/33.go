619dacd94ba294e6ef72082893b3ee79aafd6745
csxuejin <602296435@qq.com>
2020-03-22 23:37:58 +0800

    add solution for 33

--- go-solutions/33.go
+++ go-solutions/33.go
@@ -1,22 +1,49 @@
 package main
 
 func search(nums []int, target int) int {
-	if len(nums) == 0 {
+	n := len(nums)
+	switch {
+	case n == 0 || (n == 1 && nums[0] != target):
 		return -1
+	case n == 1 && nums[0] == target:
+		return 0
 	}
-	splitPos := 0
-	for i := 1; i < len(nums); i++ {
-		if nums[i] < nums[i-1] {
-			splitPos = i - 1
-			break
+
+	splitPos := findSplitPos(nums, 0, n-1)
+	switch {
+	case nums[splitPos] == target:
+		return splitPos
+
+	case splitPos == 0:
+		// no rotation
+		return binarySearch(nums, target, 0, n-1)
+
+	default:
+		if target < nums[0] {
+			return binarySearch(nums, target, splitPos, n-1)
+		}
+		return binarySearch(nums, target, 0, splitPos)
+	}
+
+	return -1
+}
+
+func findSplitPos(nums []int, left, right int) int {
+	if nums[left] < nums[right] {
+		return 0
+	}
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] > nums[mid+1] {
+			return mid + 1
+		}
+		if nums[mid] < nums[left] {
+			right = mid - 1
+		} else {
+			left = mid + 1
 		}
 	}
-
-	if pos := binarySearch(nums, target, 0, splitPos); pos != -1 {
-		return pos
-	}
-
-	return binarySearch(nums, target, splitPos+1, len(nums)-1)
+	return 0
 }
 
 func binarySearch(nums []int, target, left, right int) int {
