8a1b8facba86bab8206127142fb74f9b15d96564
csxuejin <602296435@qq.com>
2020-03-31 11:24:54 +0800

    add code

--- go-solutions/21.go
+++ go-solutions/21.go
@@ -32,14 +32,19 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 
 // method 2: use recursion
 //func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-//	if l1 == nil return l2
-//	if l2 == nil return l1
+//	if l1 == nil {
+//		return l2
+//	}
+//
+//	if l2 == nil {
+//		return l1
+//	}
 //
 //	if l1.Val < l2.Val {
 //		l1.Next = mergeTwoLists(l1.Next, l2)
 //		return l1
 //	}
 //
-//	l2.Next = mergeTwoLists(l2.Next, l1)
+//	l2.Next = mergeTwoLists(l1, l2.Next)
 //	return l2
 //}
