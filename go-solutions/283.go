60199c2017728903b8990d9e4c1ce6527b95ba95
csxuejin <602296435@qq.com>
2021-01-23 10:56:29 +0800

    refactor code

--- go-solutions/283.go
+++ go-solutions/283.go
@@ -1,15 +1,11 @@
-package main
+func moveZeroes(nums []int)  {
+    notZero := 0
+    for i:=0; i<len(nums); i++ {
+        if nums[i] == 0 {
+            continue
+        }
 
-func moveZeroes(nums []int) {
-	index := 0
-	for i := 0; i < len(nums); i++ {
-		if nums[i] == 0 {
-			continue
-		}
-
-		if index != i {
-			nums[index], nums[i] = nums[i], 0
-		}
-		index++
-	}
+        nums[notZero], nums[i] = nums[i], nums[notZero]
+        notZero++
+    }    
 }
