d14266f1c2c2ba58c9521bc0cc95521381dd2215
csxuejin <602296435@qq.com>
2021-12-12 18:54:53 +0800

    add solution for problem 200

--- golang/200/200.go
+++ golang/200/200.go
@@ -22,6 +22,17 @@ func numIslands(grid [][]byte) int {
 	return res
 }
 
+type Node struct {
+	x, y int
+}
+
+var arr = [][]int{
+	{-1, 0},
+	{1, 0},
+	{0, -1},
+	{0, 1},
+}
+
 func bfs(node Node, grid *[][]byte) {
 	m, n := len(*grid), len((*grid)[0])
 	queue := []Node{node}
@@ -46,13 +57,49 @@ func bfs(node Node, grid *[][]byte) {
 	}
 }
 
-type Node struct {
-	x, y int
+/* 也可以用 DFS 来解题，如下
+
+var visited [][]bool
+
+func numIslands(grid [][]byte) int {
+    visited = make([][]bool, len(grid))
+    for i:=0; i<len(grid); i++ {
+        visited[i] = make([]bool, len(grid[i]))
+    }
+    res := 0
+    for i:=0; i<len(grid); i++ {
+        for j:=0; j<len(grid[i]); j++ {
+            if grid[i][j] == '0' || visited[i][j] {
+                continue
+            }
+            dfs(&grid, i, j)
+            res++
+        }
+    }
+    return res
 }
 
-var arr = [][]int{
-	{-1, 0},
-	{1, 0},
-	{0, -1},
-	{0, 1},
+var arr = [][]int {
+    {-1, 0},
+    {1, 0},
+    {0, -1},
+    {0, 1},
 }
+
+func dfs(grid *[][]byte, row, col int) {
+    if row < 0 || row >= len(*grid) || col < 0 || col >= len((*grid)[0]) {
+        return
+    }
+
+    if visited[row][col] || (*grid)[row][col] == '0'{
+        return
+    }
+    visited[row][col] = true
+
+    for i:=0; i<len(arr); i++ {
+        x := row + arr[i][0]
+        y := col + arr[i][1]
+        dfs(grid, x, y)
+    }
+}
+*/
