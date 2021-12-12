1e0bfd6b6d8a9e06bf57c952ea2e26f5ab5c35b4
csxuejin <602296435@qq.com>
2021-12-12 16:43:13 +0800

    add solution for 111

--- golang/111/111.go
+++ golang/111/111.go
@@ -6,6 +6,41 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// BFS 解法如下：
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	depth := 1
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		l := len(queue)
+		for i := 0; i < l; i++ {
+			first := queue[i]
+
+			if first.Left == nil && first.Right == nil {
+				return depth
+			}
+
+			if first.Left != nil {
+				queue = append(queue, first.Left)
+			}
+
+			if first.Right != nil {
+				queue = append(queue, first.Right)
+			}
+		}
+		queue = queue[l:]
+		depth++
+	}
+
+	return depth
+}
+
+/*
+也可以用 DFS，如下：
+
 func minDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
@@ -35,3 +70,4 @@ func min(a int, b int) int {
 	}
 	return b
 }
+*/
