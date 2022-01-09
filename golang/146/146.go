6a6c44b309eeb7d7e55a833de0845c782a8a5bcf
csxuejin <602296435@qq.com>
2022-01-09 23:34:00 +0800

    refactor code

--- golang/146/146.go
+++ golang/146/146.go
@@ -1,69 +1,67 @@
 package main
 
-import "container/list"
+import (
+	"container/list"
+)
 
 type LRUCache struct {
-	cap       int
-	mapCache  map[int]*list.Element
-	listCache *list.List
+	cap   int
+	exist map[int]*list.Element
+	queue *list.List
+}
+
+func Constructor(capacity int) LRUCache {
+	if capacity < 0 {
+		capacity = 1 << 10
+	}
+
+	return LRUCache{
+		cap:   capacity,
+		exist: make(map[int]*list.Element),
+		queue: list.New(),
+	}
 }
 
 type cacheItem struct {
 	key, value int
 }
 
-var defaultLRUCacheCap = 1 << 10
-
-func Constructor(cap int) LRUCache {
-	if cap <= 0 {
-		cap = defaultLRUCacheCap
-	}
-	return LRUCache{
-		cap:       cap,
-		mapCache:  make(map[int]*list.Element),
-		listCache: list.New(),
-	}
-}
-
-// Get returns the value of the key if the key exists int the
-// cache, otherwise returns -1.
-func (lru *LRUCache) Get(key int) int {
-	v, ok := lru.mapCache[key]
+func (this *LRUCache) Get(key int) int {
+	v, ok := this.exist[key]
 	if !ok {
 		return -1
 	}
 
-	lru.listCache.MoveToFront(v)
+	this.queue.MoveToFront(v)
 	return v.Value.(*cacheItem).value
 }
 
-// Put inserts the value of the key.
-// If key exists, then substitute the value of key.
-func (lru *LRUCache) Put(key, value int) {
-	v, ok := lru.mapCache[key]
+func (this *LRUCache) Put(key int, value int) {
+	v, ok := this.exist[key]
 	if ok {
 		v.Value.(*cacheItem).value = value
-		lru.listCache.MoveToFront(v)
+		this.queue.MoveToFront(v)
 		return
 	}
 
-	if lru.Len() >= lru.cap {
-		lru.RemoveOldest()
+	if this.Len() >= this.cap {
+		this.RemoveOldest()
 	}
 
-	item := &cacheItem{key: key, value: value}
-	element := lru.listCache.PushFront(item)
-	lru.mapCache[key] = element
+	item := &cacheItem{
+		key:   key,
+		value: value,
+	}
+	element := this.queue.PushFront(item)
+	this.exist[key] = element
 }
 
-// RemoveOldest remove the oldest item of LRUCache lru.
-func (lru *LRUCache) RemoveOldest() {
-	element := lru.listCache.Back()
-	lru.listCache.Remove(element)
-	delete(lru.mapCache, element.Value.(*cacheItem).key)
+func (this *LRUCache) RemoveOldest() {
+	element := this.queue.Back()
+	this.queue.Remove(element)
+	delete(this.exist, element.Value.(*cacheItem).key)
 }
 
-// Len returns the length of LRUCache lru.
-func (lru *LRUCache) Len() int {
-	return lru.listCache.Len()
+func (this *LRUCache) Len() int {
+	return this.queue.Len()
 }
