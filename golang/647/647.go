67dda047a3dbb95b1bc2f558cfde38df11bf64d5
csxuejin <602296435@qq.com>
2022-01-20 11:36:24 +0800

    add code for 647

---
+++ golang/647/647.go
@@ -0,0 +1,30 @@
+package main
+
+func countSubstrings(s string) int {
+	str := "#"
+	for _, v := range s {
+		str += string(v) + "#"
+	}
+
+	res := 0
+	for i := range str {
+		res += countPalindrome(str, i)
+	}
+	return res - (1 + len(s))
+}
+
+func countPalindrome(s string, pos int) int {
+	left, right := pos-1, pos+1
+	cnt := 1
+	for left >= 0 && left < right && right < len(s) {
+		if s[left] != s[right] {
+			return cnt
+		}
+		if s[left] != '#' {
+			cnt++
+		}
+		left--
+		right++
+	}
+	return cnt
+}
