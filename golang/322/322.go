342d81237c9fe5b75edeb1df13665a6dc304fb85
csxuejin <602296435@qq.com>
2022-01-27 23:29:59 +0800

    add annotation for code

--- golang/322/322.go
+++ golang/322/322.go
@@ -1,6 +1,14 @@
 package main
 
-// https://leetcode-cn.com/problems/coin-change/submissions/
+/*
+题目链接: https://leetcode-cn.com/problems/coin-change/submissions/
+
+简单备注思路：
+dp 定义: dp(n) 表示要凑足金额 n 所需的最小的硬币个数，那么：
+- 当 n = 0 时， dp(n) = 0
+- 当 n < 0 时， dp(n) = -1
+- 当 n 为其他情况时，dp(n) = min(dp[n], dp[n-coin] + 1), 其中 coin 属于 coins 数组
+*/
 
 func coinChange(coins []int, amount int) int {
 	maxVal := amount + 1
