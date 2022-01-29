f6ce5879a1313198e7c1155d19d9cdf44b63ce3b
csxuejin <602296435@qq.com>
2022-01-29 11:19:44 +0800

    add annotation

--- golang/102/102.go
+++ golang/102/102.go
@@ -1,9 +1,20 @@
 package main
 
+// leetcode 对 go 的全局变量的支持有问题, 所以干脆用指针传递二维数组好了
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return [][]int{}
+	}
+	s := [][]int{}
+	bfs(&s, 0, root)
+	return s
 }
 
 func bfs(s *[][]int, level int, root *TreeNode) {
@@ -16,13 +27,4 @@ func bfs(s *[][]int, level int, root *TreeNode) {
 	(*s)[level] = append((*s)[level], root.Val)
 	bfs(s, level+1, root.Left)
 	bfs(s, level+1, root.Right)
-}
-
-func levelOrder(root *TreeNode) [][]int {
-	if root == nil {
-		return [][]int{}
-	}
-	s := [][]int{}
-	bfs(&s, 0, root)
-	return s
 }
