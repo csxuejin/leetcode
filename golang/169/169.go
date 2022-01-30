b64eb7e17fe53d277af5faf0b31e9adc3bc64243
csxuejin <602296435@qq.com>
2022-01-30 22:40:03 +0800

    add code for 169

--- golang/169/169.go
+++ golang/169/169.go
@@ -10,3 +10,28 @@ func majorityElement(nums []int) int {
 	}
 	return 0
 }
+
+/*
+方法二： 比较取巧的办法，采用 cnt 计数，
+遇到相同的数字就加一，不同的数字就减一，
+最终 cnt 大于 0 的情况是指向的数字就是出现个数大于一般的数字。
+
+func majorityElement(nums []int) int {
+    var res int
+    var cnt int
+    for _, v := range nums {
+        if cnt == 0 {
+            res = v
+            cnt = 1
+        } else {
+            if res == v {
+                cnt++
+            } else {
+                cnt--
+            }
+        }
+    }
+
+    return res
+}
+*/
