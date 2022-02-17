39a7c2d8383b3a7e4dc197f3080ec96db1c1ec1c
csxuejin <602296435@qq.com>
2022-02-17 22:13:32 +0800

    modify code

--- golang/438/438.go
+++ golang/438/438.go
@@ -1,41 +1,36 @@
 package main
 
-import "fmt"
-
 func findAnagrams(s string, p string) []int {
-	required := 0
-	requireHash := make(map[byte]int)
-	for i := 0; i < len(p); i++ {
-		if _, ok := requireHash[p[i]]; !ok {
-			required++
-		}
-		requireHash[p[i]]++
+	need := make(map[byte]int)
+	for _, v := range []byte(p) {
+		need[v]++
 	}
 
-	winSize := len(p)
-	satisfied := 0
-	curHash := make(map[byte]int)
-	res := make([]int, 0)
-	for i := 0; i < len(s); i++ {
-		curHash[s[i]]++
-		if _, ok := requireHash[s[i]]; ok && curHash[s[i]] == requireHash[s[i]] {
-			satisfied++
+	left, right := 0, 0
+	valid := 0
+	window := make(map[byte]int)
+	result := make([]int, 0)
+	for right < len(s) {
+		c := s[right]
+		window[c]++
+		right++
+
+		if window[c] == need[c] {
+			valid++
 		}
 
-		left := i - winSize
-		if left >= 0 {
-			fmt.Printf("cur window is %v\n", s[i-winSize:])
-			t := s[left]
-			curHash[t]--
-			if _, ok := requireHash[t]; ok && curHash[t] != requireHash[t] {
-				satisfied--
+		for right-left >= len(p) {
+			if valid == len(need) {
+				result = append(result, left)
 			}
-		}
-		fmt.Printf("i=%v, satisfied=%v, required=%v\n", i, satisfied, required)
 
-		if satisfied == required {
-			res = append(res, left+1)
+			c = s[left]
+			if window[c] == need[c] {
+				valid--
+			}
+			window[c]--
+			left++
 		}
 	}
-	return res
+	return result
 }
