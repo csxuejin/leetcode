ade3d6c44c2a28bf909deb289edb094d6a94b36e
csxuejin <602296435@qq.com>
2022-04-07 09:40:04 +0800

    modify code

--- golang/148/148.go
+++ golang/148/148.go
@@ -1,28 +1,57 @@
 package main
 
-import "sort"
-
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
 func sortList(head *ListNode) *ListNode {
-	arrs := make([]int, 0)
-	cur := head
-	for cur != nil {
-		arrs = append(arrs, cur.Val)
-		cur = cur.Next
-	}
-	sort.Ints(arrs)
+	return sort(head, nil)
+}
 
-	h := &ListNode{}
-	cur = h
-	for _, v := range arrs {
-		cur.Next = &ListNode{
-			Val: v,
-		}
-		cur = cur.Next
+func sort(head, tail *ListNode) *ListNode {
+	if head == nil {
+		return head
 	}
-	return h.Next
+
+	if head.Next == tail {
+		head.Next = nil
+		return head
+	}
+
+	slow, fast := head, head
+	for fast != tail {
+		slow = slow.Next
+		fast = fast.Next
+		if fast != tail {
+			fast = fast.Next
+		}
+	}
+
+	mid := slow
+	return merge(sort(head, mid), sort(mid, tail))
+}
+
+func merge(head1, head2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+	t, t1, t2 := dummy, head1, head2
+	for t1 != nil && t2 != nil {
+		if t1.Val < t2.Val {
+			t.Next = t1
+			t1 = t1.Next
+		} else {
+			t.Next = t2
+			t2 = t2.Next
+		}
+
+		t = t.Next
+	}
+
+	if t1 == nil {
+		t.Next = t2
+	} else if t2 == nil {
+		t.Next = t1
+	}
+
+	return dummy.Next
 }
