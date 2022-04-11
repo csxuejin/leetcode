36926a3ff4a60e90a442998a948e1b2671ef7074
csxuejin <602296435@qq.com>
2022-04-11 13:54:56 +0800

    add code

--- golang/94/94.go
+++ golang/94/94.go
@@ -20,3 +20,25 @@ func inorderTraversal(root *TreeNode) []int {
 
 	return res
 }
+
+// 也有可能考察非递归的做法
+/*
+func inorderTraversal(root *TreeNode) []int {
+	stack := make([]*TreeNode, 0)
+	res := make([]int, 0)
+
+	for root != nil || len(stack) != 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, root.Val)
+		root = root.Right
+	}
+
+	return res
+}
+*/
