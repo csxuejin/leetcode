0cfa6724ec8e4ed31fe7ce3abe205f483d9e8be7
csxuejin <602296435@qq.com>
2022-04-12 16:19:12 +0800

    add code

--- golang/1143/1143.go
+++ golang/1143/1143.go
@@ -16,6 +16,7 @@ func longestCommonSubsequence(text1 string, text2 string) int {
 	return dp(text1, text2, 0, 0)
 }
 
+// 递归解法
 func dp(text1, text2 string, i, j int) int {
 	if i == len(text1) || j == len(text2) {
 		return 0
@@ -40,3 +41,27 @@ func max(a, b int) int {
 	}
 	return b
 }
+
+// dp 解法
+/*
+func longestCommonSubsequence(text1 string, text2 string) int {
+    dp := make([][]int, len(text1)+1)
+    for i :=0; i<= len(text1); i++ {
+        dp[i] = make([]int, len(text2)+1)
+    }
+
+    m, n := len(text1), len(text2)
+
+    for i:=1; i<=m; i++ {
+       for j:=1; j<=n; j++ {
+            if text1[i-1] == text2[j-1] {
+                dp[i][j] = dp[i-1][j-1] + 1
+            } else {
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            }
+        }
+    }
+
+    return dp[m][n]
+}
+*/
